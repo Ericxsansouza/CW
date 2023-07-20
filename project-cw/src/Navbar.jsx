1c42f2a02a7e504fc00fcbe249a7ab127de2c4cf
Ericxsansouza <erick.sousa@sptech.school>
2023-07-19 21:04:39 -0300

    Subindo a primeira versão

---
+++ project-cw/src/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Navbar.css';
+import './styleGlobal.css';
+
+export default function Navbar() {
+    return (
+        <header className="container">
+            <div className="content">
+                
+            </div>
+        </header>
+    )
+}
