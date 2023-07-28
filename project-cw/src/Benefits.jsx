b8c5884a8f2f45eb701420c28dde47fb61c2fcb3
Ericxsansouza <erick.sousa@sptech.school>
2023-07-28 20:53:55 -0300

    teste4

--- project-cw/src/Benefits.jsx
+++ project-cw/src/Benefits.jsx
@@ -13,35 +13,59 @@ function Benefits() {
             </div>
             <div className='content-benefits-card-one'>
                 <div className='card'>
-                    <img src='./public/relogio.png'></img>
-                    <h3>Análise de crédito em tempo real</h3>
-                    <p>Ao fornecer resultados rápidos e confiáveis, os clientes têm a vantagem de tomar decisões financeiras mais rápidas.</p>
+                    <div className='content-card'>
+                        <div className='iconBox'>
+                            <img src='./public/relogio.png'></img>
+                        </div>
+                        <h3>Análise de crédito em tempo real</h3>
+                        <div className='MuiTypography'>
+                            <p>Ao fornecer resultados rápidos e confiáveis, os clientes têm a vantagem de tomar decisões financeiras mais rápidas.</p>
+                        </div>
+                    </div>
                     <div className='to-know'>
                         <a href='/'>CONHECER</a><img src='./public/jogar.png'></img>
                     </div>
                 </div>
                 <div className='card'>
-                    <img src='./public/nano-tecnologia.png'></img>
-                    <h3>Tecnologia de inteligência</h3>
-                    <p>Utilizamos tecnologia de ponta baseada em inteligência artificial e aprendizado de máquina.</p>
+                    <div className='content-card'>
+                        <div className='iconBox'>
+                            <img src='./public/nano-tecnologia.png'></img>
+                        </div>
+                        <h3>Tecnologia de inteligência</h3>
+                        <div className='MuiTypography'>
+                            <p>Utilizamos tecnologia de ponta baseada em inteligência artificial e aprendizado de máquina.</p>
+                        </div>
+                    </div>
                     <div className='to-know'>
                         <a href='/'>CONHECER</a><img src='./public/jogar.png'></img>
                     </div>
                 </div>
             </div>
             <div className='content-benefits-card-two'>
-                <div className='card'>
-                    <img src='./public/transparencia.png'></img>
-                    <h3>Personalizada e flexível</h3>
-                    <p>Adaptamos nossos serviços às necessidades específicas de cada cliente, garantindo soluções sob medida.</p>
+            <div className='card'>
+                    <div className='content-card'>
+                        <div className='iconBox'>
+                            <img src='./public/transparencia.png'></img>
+                        </div>
+                        <h3>Personalizada e flexível</h3>
+                        <div className='MuiTypography'>
+                            <p>Adaptamos nossos serviços às necessidades específicas de cada cliente, garantindo soluções sob medida.</p>
+                        </div>
+                    </div>
                     <div className='to-know'>
                         <a href='/'>CONHECER</a><img src='./public/jogar.png'></img>
                     </div>
                 </div>
                 <div className='card'>
-                    <img src='./public/editar.png'></img>
-                    <h3>Transparência e assessoria especializada</h3>
-                    <p>Compromisso com a transparência e pela oferta de assessoria especializada.</p>
+                    <div className='content-card'>
+                        <div className='iconBox'>
+                            <img src='./public/editar.png'></img>
+                        </div>
+                        <h3>Transparência e assessoria especializada</h3>
+                        <div className='MuiTypography'>
+                            <p>Compromisso com a transparência e pela oferta de assessoria especializada.</p>
+                        </div>
+                    </div>
                     <div className='to-know'>
                         <a href='/'>CONHECER</a><img src='./public/jogar.png'></img>
                     </div>
